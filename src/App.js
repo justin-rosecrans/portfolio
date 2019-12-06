f839f74275b03188cb61aa5b0091c7c0bf9b238f
Justin Rosecrans <justin.rosecrans@tdameritrade.com>
2019-12-06 10:03:55 -0600

    banner-footer-nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './Navigation/Navigation';
+import Banner from './Banner/Banner';
+import Footer from './Footer/Footer';
 import './App.css';
+import 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Navigation />
+      <Banner />
+      <Footer />
     </div>
   );
 }
---
+++ src/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Banner.scss';
+
+class Banner extends Component {
+    render() {
+        return (
+            <section className="banner"></section>
+        )
+    }
+}
+
+export default Banner;
